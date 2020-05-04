a1b56015f211c1a33261bc3e5d4b16b5deb638a4
jerwyk <jerwyk0526@gmail.com>
2020-05-04 13:53:34 -0400

    moved gdt and idr from init to kernel

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -3,10 +3,39 @@
 */
 
 #include "kernel.h"
+#include <kernel/printf.h>
+#include <kernel/gdt.h>
+#include <kernel/interrupt.h>
+#include <hardware/keyboard.h>
+#include <hardware/driver.h>
+#include <hardware/console.h>
+
+using namespace MoeP;
+using namespace MoeP::kernel;
+
+static bool shutdown = false;
 
 extern void kernelMain()
 {
-    MoeP::kernel::printf("kernel booted");
-    while(true);
-}
+    printf("Welcome to MoeP!\n");
+    printf("This is a simple OS.\n");
+    //initialization
+    //setup global descriptor table
+    GlobalDescriptorTable gdt = GlobalDescriptorTable();
+    //setup interrupts and interrupt descriptor table
+    InterruptManager interrupt = InterruptManager(&gdt);
+    hardware::DriverManager drv = hardware::DriverManager();
+    //initialize hardware
+    hardware::KeyboardDriver keyboard(&interrupt);
+    drv.AddDriver(&keyboard);
 
+    hardware::ConsoleDriver console;
+    console.Activate();
+
+    interrupt.Activate();
+    
+    while(shutdown)
+    {
+
+    }
+}
